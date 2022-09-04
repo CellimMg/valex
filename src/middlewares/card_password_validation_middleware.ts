f3bf23643c30400c81fc584f6651fbf992b43c83
marcelo <cello.ma16@gmail.com>
2022-09-04 20:54:04 -0300

    [feature] adiciona middleware de validação de senha na requisição

---
+++ src/middlewares/card_password_validation_middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { Card } from "../repositories/card_repository";
+
+export async function cardPasswordValidation(req: Request, res: Response, next: NextFunction) {
+    const { password } = req.body;
+    const card: Card = res.locals.card;
+
+    if (!bcrypt.compareSync(password, card.password!)) return res.status(400).send({ message: "Ops! A senha do cartão está incorreta!" });
+
+    next();
+}
